36e146e1d5b9bc838ae14d010f7a615d5f579907
Dominik Marsanic <dominik.marsanic@predavaci.algebra.hr>
2025-03-10 14:53:08 +0100

    Prvi testni commit i push

---
+++ 06032025/modulo_operaciju.py
@@ -0,0 +1,11 @@
+print(10 % 3) # Ispisuje 1, jer je 10 podijeljeno s 3 jedanako 3 s ostatkom 1
+print(6 % 2) # Ispisuje 0, jer je 6 podijeljeno s 2 jedanako 2 s ostatkom 0
+print(7 % 5)
+
+broj = 56
+if broj % 2 == 0:
+    print('Broj je paran.')
+else:
+    print('Broj je neparan.')
+
+60 % 3
