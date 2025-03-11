ff640a94c8c99da1ed68b0422e2e2a46bda21ae0
dmarsanic2025 <dominik.marsanic@predavaci.algebra.hr>
2025-03-11 18:36:58 +0100

    Fixan naziv varijable

--- 10032025/DZ/dz.py
+++ 10032025/DZ/dz.py
@@ -8,11 +8,11 @@ automobilom koji je udaljen 20 km u jednom smjeru.
 potrosnja_na_100km = 5.3
 cijena_po_litri = 9.56
 udaljenost_u_oba_smjera = 20 * 2
-dani_u_mjesecu = 30
+BROJ_DANA_U_MJESECU = 30
 
 trosak_po_km = (potrosnja_na_100km / 100) * cijena_po_litri
 
-mjesecni_trosak = trosak_po_km * udaljenost_u_oba_smjera * dani_u_mjesecu
+mjesecni_trosak = trosak_po_km * udaljenost_u_oba_smjera * BROJ_DANA_U_MJESECU
 
 
 print(mjesecni_trosak, "kn")
